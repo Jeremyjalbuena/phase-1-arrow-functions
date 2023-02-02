384814b59c8be7f80b03b32635fcf399fdc3b45d
Jeremy Jalbuena <jalbuena_jeremy@yahoo.com>
2023-02-02 10:09:36 -0500

    did the lab

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,100);
+
+const square = (parameter) => parameter*parameter;
+square(parameter);
+
+let add = (parameter3,parameter4) => parameter3+parameter4;
+add(parameter3,parameter4);
